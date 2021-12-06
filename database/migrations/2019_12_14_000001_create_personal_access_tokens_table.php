0e4773635cb3e075d5b5a57d46846813d61a063c
teshima0903 <y.teshima0903@gmail.com>
2021-12-06 19:52:44 +0900

    ->charset("utf8") 追記

---
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePersonalAccessTokensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('personal_access_tokens', function (Blueprint $table) {
+            $table->id()->charset("utf8");
+            $table->morphs('tokenable')->charset("utf8");
+            $table->string('name')->charset("utf8");
+            $table->string('token', 64)->charset("utf8")->unique();
+            $table->text('abilities')->nullable();
+            $table->timestamp('last_used_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('personal_access_tokens');
+    }
+}
